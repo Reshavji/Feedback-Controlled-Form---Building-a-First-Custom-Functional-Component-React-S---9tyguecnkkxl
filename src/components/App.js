010fe179fcf575e1dc47700b6ed68ebcf2702fcc
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-05-20 21:27:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,58 @@
-import React, { useState } from 'react'
+
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+const [rating, setRating] = useState(5);
+const [comment, setComment] = useState('');
+const [commentError, setCommentError] = useState('');
 
-  return (
-    <div id="main">
-      <form>
-        <div>
-          <label htmlFor='rating'>Rating: </label>
-          <input 
-            type="range"
-            min="1"
-            max="10"
-            id="rating"
-          />
-          <span className='rating'>rating</span>
-        </div>
-        <div>
-          <label htmlFor='comment'>Comment: </label>
-          <textarea 
-            id='comment'
-          />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
-        </div>
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
-  )
+const handleRatingChange = (event) => {
+setRating(event.target.value);
+};
+
+const handleCommentChange = (event) => {
+setComment(event.target.value);
+if (event.target.value.length < 5) {
+setCommentError('Comment must be at least 5 characters.');
+} else {
+setCommentError('');
 }
+};
 
+const handleSubmit = (event) => {
+event.preventDefault();
+// Perform submit action here
+};
+
+return (
+<div id="main">
+<form onSubmit={handleSubmit}>
+<div>
+<label htmlFor='rating'>Rating: </label>
+<input 
+         type="range"
+         min="1"
+         max="10"
+         id="rating"
+         value={rating}
+         onChange={handleRatingChange}
+       />
+<span className='rating'>{rating}</span>
+</div>
+<div>
+<label htmlFor='comment'>Comment: </label>
+<textarea 
+         id='comment'
+         value={comment}
+         onChange={handleCommentChange}
+       />
+<p style={{ color: 'red' }} className="comment-error">{commentError}</p>
+</div>
+<button type='submit'>Submit</button>
+</form>
+</div>
+)
+}
 
 export default App;
